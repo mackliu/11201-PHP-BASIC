fcc17f7a1156513bc563afbee45f6a789b751a85
mackliu <macklun@gmail.com>
2023-04-14 13:55:15 +0800

    流程控制-多選switch...case

--- flow/index.php
+++ flow/index.php
@@ -1,5 +1,5 @@
 <?php
-$score=50;
+$score=90;
 
 if($score>60){
     echo "成績=".$score;
@@ -11,4 +11,24 @@ if($score>60){
     echo "請再接再厲~~";
 }
 
+echo "<hr>";
 
+$level="B";
+
+switch($level){
+    case "A":
+        echo "表現優良，請繼續保持";
+    break;
+    case "B":
+        echo "值得肯定，還有進步空間";
+    break;
+    case "C":
+        echo "需要更多的練習";
+    break;
+    case "D":
+        echo "需要加強基本功";
+    break;
+    default:
+        echo "是否無心學業?";
+
+}
