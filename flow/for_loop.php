b02fe1c711e4702b8b9c01a46b6e2ffe9b73f8a0
mackliu <macklun@gmail.com>
2023-04-14 16:08:39 +0800

    流程控制-練習回圈數列1

--- flow/for_loop.php
+++ flow/for_loop.php
@@ -6,3 +6,26 @@ for($i=0;$i<10;$i++){
 }
 
 echo '$i='.$i;
+
+
+echo "<hr>";
+$n=21;
+//$limit=ceil($n/2);
+for($i=1;$i<=ceil($n/2);$i++){
+    //echo '$i=' . $i . "=>";
+    echo $i*2-1;
+    echo ",";
+}
+echo "<br>";
+
+echo "<hr>";
+$n=21;
+//$limit=ceil($n/2);
+for($i=1;$i<=$n;$i=$i+2){
+    //echo '$i=' . $i . "=>";
+    echo $i;
+    echo ",";
+}
+echo "<br>";
+
+echo "<hr>";
