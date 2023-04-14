c874b51806468fede814d745d055f6b6f3019a19
mackliu <macklun@gmail.com>
2023-04-14 09:17:45 +0800

    PHP基礎:變數的宣告和使用

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$name='mary';
+$age=24;
+$total=15*110;
+
+echo "<span style='color:blue;font-size:24px'>";
+echo "名字是：";
+echo $name;
+echo "</span>";
+echo "<span style='color:green;font-size:24px'><br>";
+echo "年齡是：";
+echo $age;
+echo "</span>";
+echo "<span style='color:red;font-size:24px'><br>";
+echo "總價是：";
+echo $total;
+echo "</span>";
+
+
