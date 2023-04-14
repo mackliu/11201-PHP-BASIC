0cdac74fd69e58b73da22424c709f9aa7a9292a3
mackliu <macklun@gmail.com>
2023-04-14 16:28:34 +0800

    流程控制-練習回圈數列2-找質數

--- flow/for_loop.php
+++ flow/for_loop.php
@@ -29,3 +29,41 @@ for($i=1;$i<=$n;$i=$i+2){
 echo "<br>";
 
 echo "<hr>";
+//質數判斷方法
+$n=96;
+$check=false;
+for($i=2;$i<$n;$i++){
+
+    if($n%$i==0){
+        $check=true;
+        break;
+    }
+
+}
+
+if($check==false){
+    echo $n . "是質數";
+}else{
+    echo $n . "不是質數";
+}
+
+
+echo "<hr>";
+$n=97;
+
+for($i=3;$i<=97;$i++){
+    $check=false;
+    for($j=2;$j<$i;$j++){
+    
+        if($i%$j==0){
+            $check=true;
+            break;
+        }
+    }
+    
+    if($check==false){
+        echo $i . ",";
+    }
+}
+
+
