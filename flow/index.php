c5321f2d47bc767ea306c00cb522acfa0d9c0a6f
mackliu <macklun@gmail.com>
2023-04-14 13:36:57 +0800

    流程控制-if...else

--- flow/index.php
+++ flow/index.php
@@ -1,4 +1,14 @@
 <?php
+$score=50;
+
+if($score>60){
+    echo "成績=".$score;
+    echo "<br>";
+    echo "恭喜及格了~~";
+}else{
+    echo "成績=".$score;
+    echo "<br>";
+    echo "請再接再厲~~";
+}
 
 
-echo "123";
