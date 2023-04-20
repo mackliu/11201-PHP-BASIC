2f70c747a9808c7d162e65ba5ed7420cc10f4bd7
mackliu <macklun@gmail.com>
2023-04-20 13:19:27 +0800

    特殊九九乘法表練習

--- flow/pra2.php
+++ flow/pra2.php
@@ -11,6 +11,11 @@
         td {
             border: 1px solid lightgray; /* 設定 td 的線為淺灰色 */
             padding: 5px; /* 設定 td 內容的 padding 為 5px */
+            text-align: center;
+        }
+        td:nth-child(1),
+        tr:nth-child(1){
+            background-color: #ccc;
         }
     </style>
 </head>
@@ -24,6 +29,35 @@
             for ($j = 1; $j <= 9; $j++) {
                 // 使用 <td> 元素代表表格中的一個儲存格，印出乘法表中的一項
                 echo "<td>" . $i . " x " . $j . " = " . $i * $j . "</td>";
+            }
+            // 使用 </tr> 元素代表表格中的一行結束
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
+    <table>
+        <?php
+        // 使用 for 迴圈產生九九乘法表，$i 代表第一個數字，$j 代表第二個數字
+        for ($i = 0; $i <= 9; $i++) {
+            // 使用 <tr> 元素代表表格中的一行
+            echo "<tr>";
+            for ($j = 0; $j <= 9; $j++) {
+
+                echo "<td>";
+                //  根據不同的條件，來決定每一格中要出現的內容
+                    if($i==0 && $j==0){
+                        echo "&nbsp;";
+                    }else if($i==0){
+                        echo $j;
+                    }else if($j==0){
+                        echo $i;
+                    }else{
+                        echo $i * $j;
+                    }
+                echo "</td>";
+
+                //echo "<td>" . $i . " x " . $j . " = " . $i * $j . "</td>";
             }
             // 使用 </tr> 元素代表表格中的一行結束
             echo "</tr>";
