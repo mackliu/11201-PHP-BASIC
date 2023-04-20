c5bd2d8282c22735369a1677c387ac3d1856601c
mackliu <macklun@gmail.com>
2023-04-20 14:12:33 +0800

    正三角形

--- flow/starts.php
+++ flow/starts.php
@@ -1,3 +1,8 @@
+<style>
+    *{
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
 <h3>直角三角形</h3>
 <?php
 for($i=0;$i<5;$i++){
@@ -10,5 +15,30 @@ for($i=0;$i<5;$i++){
 }
 ?>
 
+<h3>倒直角三角形</h3>
 
+<?php
+for($i=0;$i<5;$i++){
 
+    for($j=0;$j<(5-$i);$j++){
+        echo "*";
+    }
+    echo "<br>";
+
+}
+?>
+<h3>正三角形</h3>
+<?php
+
+for($i=0;$i<5;$i++){
+    for($j=0;$j<(4-$i);$j++){
+        echo "&nbsp;";
+    }
+
+    for($k=0;$k<($i*2+1);$k++){
+        echo "*";
+    }
+    echo "<br>";
+}
+
+?>
