504e003f99fa1694b99bd1cd75b1d2c571a63ed7
mackliu <macklun@gmail.com>
2023-04-21 14:10:50 +0800

    陣列應用-威力彩選號不重覆

--- array/pra02.php
+++ array/pra02.php
@@ -1,3 +1,21 @@
+<style>
+    .area-1,.area-2{
+        display:inline-block;
+        width:35px;
+        height:35px;
+        border:1px solid #ccc;
+        line-height: 33px;
+        text-align: center;
+        border-radius: 50%;
+        margin:0.5rem;
+    }
+    .area-1{
+        background-color: #0f0;
+    }
+    .area-2{
+        background-color: orange;
+    }
+</style>
 <?php
 $nine=[];
 for($i=1;$i<=9;$i++){
@@ -15,3 +33,36 @@ foreach($nine as $k => $str){
         echo "<br>";
     }
 }
+
+echo "<hr>";
+echo "<h1>威力彩選號</h1>";
+
+$lotto=[];
+
+while(count($lotto)<6){
+    $tmp=rand(1,38);
+
+   // echo "亂數產生的數字:".$tmp;
+    if(!in_array($tmp,$lotto)){
+        //echo "沒有重覆的數字,所以寫入陣列".$tmp;
+        $lotto[]=$tmp;
+    }/* else{
+        //echo "有發生重覆的數字".$tmp;
+    } */
+/*     echo "<pre>";
+    print_r($lotto);
+    echo "</pre>"; */
+}
+
+foreach($lotto as $num){
+    echo "<span class='area-1'>" . $num . '</span>';
+}
+echo "<br>";
+echo "<span class='area-2'>".rand(1,8)."</span>";
+
+
+echo "<p>&nbsp;</p>";
+echo "<p>&nbsp;</p>";
+echo "<p>&nbsp;</p>";
+echo "<p>&nbsp;</p>";
+echo "<p>&nbsp;</p>";
