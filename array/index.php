bb9a6074259ccc8ce3bf67d62a715d9d1d5a648f
mackliu <macklun@gmail.com>
2023-04-21 09:07:43 +0800

    陣列的資料增加與取用方式

--- array/index.php
+++ array/index.php
@@ -1,14 +1,37 @@
 <?php
 
 $a[1]="A";
+$a['001']="B";
+$a['name']="mack";
+$a['parent_name']="mack";
+//$a='cdef';
+echo "<pre>";
+print_r($a);
+echo "</pre>";
 
 
 $b[]="B";
-
+$b[]="C";
+$b[]="D";
+$b[]="E";
+$b[]="F";
+echo "<pre>";
+print_r($b);
+echo "</pre>";
 
 $c=["A","B",'C'];
+$c[]="D";
+$c['total']=4;
+$c[]="E";
+$c['total']=5;
+echo "<pre>";
+print_r($c);
+echo "</pre>";
 
 echo $a[1];
+echo $a['001'];
+echo $a['name'];
+echo $a['parent_name'];
 echo "<br>";
 echo $b[0];
 echo "<br>";
