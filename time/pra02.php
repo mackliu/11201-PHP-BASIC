f96a01a9959b8aeadc893189517dda59608ec555
mackliu <macklun@gmail.com>
2023-04-24 15:53:31 +0800

    依據收集的資訊,使用迴圈畫出一個空的月曆

--- time/pra02.php
+++ time/pra02.php
@@ -1,3 +1,12 @@
+<style>
+    table{
+        border-collapse: collapse;
+    }
+    td{
+        border:1px solid gray;
+        padding:5px 10px;
+    }
+</style>
 <h2>月曆</h2>
 <?php
 $today=strtotime("now");
@@ -6,6 +15,7 @@ $days=date("t");
 $firstDate=date("Y-n-1");
 $finalDate=date("Y-n-t");
 $firstDateWeek=date("w",strtotime($firstDate));
+$weeks=ceil(($days+$firstDateWeek)/7);
 echo "月:".$month;
 echo "<br>";
 echo "天數:".$days;
@@ -16,6 +26,26 @@ echo "最後1天:".$finalDate;
 echo "<br>";
 echo "第1天星期:".$firstDateWeek;
 echo "<br>";
+echo "周數:".$weeks;
+echo "<br>";
 
+echo "<table>";
+echo "<tr>";
+echo "<td>日</td>";
+echo "<td>一</td>";
+echo "<td>二</td>";
+echo "<td>三</td>";
+echo "<td>四</td>";
+echo "<td>五</td>";
+echo "<td>六</td>";
+echo "</tr>";
+for($i=0;$i<$weeks;$i++){
+    echo "<tr>";
+    for($j=0;$j<7;$j++){
+        echo "<td>";
 
-
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
